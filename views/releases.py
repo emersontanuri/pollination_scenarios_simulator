c4bee73ac8bd763011e7fd5267ff883a3a3b9b6a
Emerson Tanuri <emersontanuri1@gmail.com>
2025-02-10 20:01:16 -0300

    Mudança no texto do release

--- views/releases.py
+++ views/releases.py
@@ -6,7 +6,7 @@ def releases():
   st.markdown('# ✨ Novidades ✨')
 
   st.markdown('### Versão 1.2.0')
-  st.write('🛠️ Agora você pode definir a média de produtividade de uma mão-de-obra (por dia) para ter a distribuição de pessoas necessárias para o trabalho.')
+  st.write('🛠️ Agora você pode definir a média de produtividade de uma mão-de-obra (por dia) para ter a distribuição de pessoas necessárias para o trabalho!')
 
   st.markdown('### Versão 1.1.0')
   st.write('⏱️ Otimização do tempo de geração de amostras de dados, tornando o processo mais rápido e eficiente.')
